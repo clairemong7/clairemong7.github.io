fad34d59e7d05b638f5eb2e738dd7beec99f4430
clairemong7 <mingma@uw.edu>
2023-12-17 17:00:25 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello world!
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
